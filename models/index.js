bf2711856dfa7c815a82d6cda5255d40b135ab10
Gustavo Silva <gustagui.dev@gmail.com>
2021-10-27 21:59:18 -0300

    conect sequelize

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require("../config/database")["development"];
+const config = require("../config/database")[development];
 const db = {};
 
 let sequelize;
