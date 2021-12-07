a48bba1ca8c7e70e7490cdb4c8a71ebf30a52f8a
Vitoria Gonçalves <vitoria.goncalvess.gaspar@gmail.com>
2021-12-07 00:08:48 -0300

    Corrigindo erro ao cadastrar imagem

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const produtoController = require('../controllers/produtoController');
 const routes = express.Router();
-const upload = require('../middlewares/upload');
+const uploadProduct = require('../middlewares/uploadProduct');
 const isLogin = require('../middlewares/isLogin');
 
 routes.get('/admin/cadastro-produto', isLogin, produtoController.cadastrarProduto);
 routes.get('/admin/produtosAdmin', isLogin, produtoController.listarProdutosAdmin);
-routes.post('/salvar-produto', isLogin, upload.single("image"), produtoController.salvarProduto);
+routes.post('/salvar-produto', isLogin, uploadProduct.single("image"), produtoController.salvarProduto);
 routes.delete('/deletar-produto/:id', isLogin, produtoController.deletarProduto);
 routes.get('/editar-produto/:id', isLogin, produtoController.editarProduto)
-routes.put('/salvar-edicao-produto/:id', isLogin, upload.single("image"), produtoController.salvarEdicao)
+routes.put('/salvar-edicao-produto/:id', isLogin, uploadProduct.single("image"), produtoController.salvarEdicao)
 
 routes.get('/produtoModels/:id', (req, res) =>{
     if(typeof Number(req.params.id) != "number") {
